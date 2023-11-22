39f1def31c5797092af0859a523772738e5431dd
Charles <Charlessammarco@gmail.com>
2023-11-21 21:55:48 -0600

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
 # your code goes here!
+import ipdb
+
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, match_list):
+        returnList = []
+        newWord = sorted([letter for letter in self.word])
+
+        for n in match_list:
+            matcher = sorted([letter for letter in n])
+
+            if newWord == matcher:
+                returnList.append(n)
+                
+        return returnList
+
+            
+
+
+
